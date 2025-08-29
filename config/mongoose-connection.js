fd8d54cdef68664d9970eb3efe7caebc1847345e
yuvraj <yuvraj.as9001@gmail.com>
2025-08-29 19:02:49 +0530

    Remove config package and switch to dotenv for MONGODB_URI

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose');
-const config=require('config');
+
 
 const dbgr= require("debug")("development:mongoose")
 
-mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)
+mongoose.connect(`${process.env.MONGODB_URI}/scatch`)
 .then(function(){
     dbgr("Connected");
 })
